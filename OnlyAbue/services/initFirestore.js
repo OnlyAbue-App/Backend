68b040b9dd6ecc96956e0f52cc1472be81babeed
JessDuff <jess.tasstic@gmail.com>
2024-09-23 20:12:00 -0400

    Modificacion de campos adicionales

--- OnlyAbue/services/initFirestore.js
+++ OnlyAbue/services/initFirestore.js
@@ -3,12 +3,55 @@ import { collection, doc, setDoc } from 'firebase/firestore';
 
 const initializeCollections = async () => {
   try {
-    await setDoc(doc(collection(firestore, 'usuarios'), 'placeholder'), {});
-    await setDoc(doc(collection(firestore, 'medicamentos'), 'placeholder'), {});
-    await setDoc(doc(collection(firestore, 'recordatorios'), 'placeholder'), {});
-    await setDoc(doc(collection(firestore, 'historial_medicamentos'), 'placeholder'), {});
-    await setDoc(doc(collection(firestore, 'citas_medicas'), 'placeholder'), {});
-    await setDoc(doc(collection(firestore, 'recomendaciones'), 'placeholder'), {});
+    await setDoc(doc(collection(firestore, 'usuarios'), 'placeholder'), {
+      nombre: "",
+      email: "",
+      fechaNacimiento: "",
+      genero: "",
+      creadoEn: new Date(),
+    });
+    
+    await setDoc(doc(collection(firestore, 'medicamentos'), 'placeholder'), {
+      nombre: "",
+      dosis: "",
+      dias: [],  
+      horas: [],
+      imagen: "",
+      color: "",
+    });
+
+    await setDoc(doc(collection(firestore, 'recordatorios'), 'placeholder'), {
+      tipo: ["medicamento", "cita medica"],
+      fecha: "",
+      hora: "",
+      repeticion: "",
+      usuarioId: "",
+      medicamentoId: "",
+      creadoEn: new Date(),
+    });
+
+    await setDoc(doc(collection(firestore, 'historial_medicamentos'), 'placeholder'), {
+      usuarioId: "",
+      medicamentoId: "",
+      fecha: "",
+      dosisTomada: "",
+      observaciones: "",
+    });
+
+    await setDoc(doc(collection(firestore, 'citas_medicas'), 'placeholder'), {
+      usuarioId: "",
+      fecha: "",
+      hora: "",
+      doctor: "",
+      especialidad: "",
+      lugar: "",
+      creadoEn: new Date(),
+    });
+
+    await setDoc(doc(collection(firestore, 'recomendaciones'), 'placeholder'), {
+      medicamentoId: "",
+      usuarioId: "",
+    });
 
     console.log('Colecciones inicializadas exitosamente');
   } catch (error) {
